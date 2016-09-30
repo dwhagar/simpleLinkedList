bc8553bfe37cc75f337fdf595334ea890db5dea9
dwhagar <cyclops@blazingumbra.com>
2016-09-30 12:57:57 -0400

    Initial Commit

---
+++ simpleLinkedList/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  simpleLinkedList
+//
+//  Created by David Wade Hagar on 9/30/16.
+//  Copyright © 2016 David Wade Hagar. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
